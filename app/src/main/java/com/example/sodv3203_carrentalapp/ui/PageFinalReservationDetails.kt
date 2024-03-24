d157a11785353a8861ef1e3cc490deb07bd9186c
Daniel Kakiuthi <daniel.kakiuthi@gmail.com>
2024-03-23 23:38:55 -0600

    feat: added layout of finalReservationDetails and summary pages

--- app/src/main/java/com/example/sodv3203_carrentalapp/ui/PageFinalReservationDetails.kt
+++ app/src/main/java/com/example/sodv3203_carrentalapp/ui/PageFinalReservationDetails.kt
@@ -6,16 +6,31 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sodv3203_carrentalapp.R
 import com.example.sodv3203_carrentalapp.data.AppUiState
 import com.example.sodv3203_carrentalapp.ui.theme.SODV3203_CarRentalAppTheme
@@ -30,8 +45,209 @@ fun DisplayPageFinalReservationDetails(
 
     Column(
         modifier = modifier
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(text = "Placeholder: Final Reservation Details Page")
+
+        Column {
+            Text(
+                text = "Complete Reservation",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = "Pickup Location",
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("Pickup Location")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {  }
+                ),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "From Date")
+                }
+            )
+            OutlinedTextField(
+                value = "Fri, Jan 6, 12:00PM",
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("From Date")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {  }
+                ),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Filled.DateRange, contentDescription = "From Date")
+                }
+            )
+            OutlinedTextField(
+                value = "Sun, Jan 8, 12:00PM",
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("To Date")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {  }
+                ),
+                trailingIcon = {
+                    Icon(imageVector = Icons.Filled.DateRange, contentDescription = "To Date")
+                }
+            )
+        }
+
+        Column {
+            Text(text = "Aditional Requests", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            OutlinedTextField(
+                value = "Eg. car model, color, etc",
+                singleLine = false,
+                minLines = 4,
+                maxLines = 4,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("Pickup Location")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { }
+                )
+            )
+        }
+
+
+        Column {
+            Text(text = "Payment", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            OutlinedTextField(
+                value = "John Doe",
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("Name on Card")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { }
+                )
+            )
+            OutlinedTextField(
+                value = "1111 2222 3333 4444",
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("Card Number")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { }
+                )
+            )
+            OutlinedTextField(
+                value = "123",
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = {},
+                label = {
+                    Text("CVC")
+                },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { }
+                )
+            )
+        }
+
+
+
         Row(
             modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
         ) {
--- app/src/main/java/com/example/sodv3203_carrentalapp/ui/PageSummary.kt
+++ app/src/main/java/com/example/sodv3203_carrentalapp/ui/PageSummary.kt
@@ -3,21 +3,38 @@ package com.example.sodv3203_carrentalapp.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.rounded.LocationOn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sodv3203_carrentalapp.R
 import com.example.sodv3203_carrentalapp.data.AppUiState
+import com.example.sodv3203_carrentalapp.data.cars
 import com.example.sodv3203_carrentalapp.ui.theme.SODV3203_CarRentalAppTheme
 
 @Composable
@@ -31,7 +48,55 @@ fun DisplayPageSummary(
     Column(
         modifier = modifier
     ) {
-        Text(text = "Placeholder: Summary Page")
+        Text(
+            text = "Order Summary",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "Ford Focus or similar", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Text(text = "$51/day, 2 days")
+        }
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "Add-Ons", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Card_Addons(title = "Audio system", information = stringResource(cars[0].category))
+                Card_Addons(title = "Connectivity", information = stringResource(cars[0].feature))
+            }
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Card_Addons(title = "Audio system", information = stringResource(cars[0].category))
+                Card_Addons(title = "Connectivity", information = stringResource(cars[0].feature))
+            }
+        }
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = "Pickup & Dropoff", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Card_PickupDropoff(location="Calgary", datetime="Fri, Jan 6, 12:00PM")
+            Icon(
+                imageVector =  Icons.Filled.MoreVert,
+                contentDescription = "Location"
+            )
+            Icon(
+                imageVector =  Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Location"
+            )
+            Card_PickupDropoff(location="Edmonton", datetime="Sun, Jan 8, 12:00PM")
+        }
+
         Row(
             modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
         ) {
@@ -53,6 +118,82 @@ fun DisplayPageSummary(
             }
         }
     }
+}
+
+
+@Composable
+private fun Card_Addons(
+    title: String,
+    information: String
+) {
+    Card(
+        modifier = Modifier
+            .width(160.dp)
+            .height(80.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .padding(vertical = 5.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(start = 16.dp)
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = information,
+                color = MaterialTheme.colorScheme.surfaceTint
+            )
+        }
+    }
+}
+
+@Composable
+private fun Card_PickupDropoff(
+    location: String,
+    datetime: String
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .padding(5.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        ) {
+            Icon(
+                imageVector =  Icons.Rounded.LocationOn,
+                contentDescription = "Location",
+                modifier = Modifier.fillMaxHeight().width(30.dp)
+            )
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(start = 16.dp)
+            ) {
+                Text(
+                    text = location,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = datetime,
+                    color = MaterialTheme.colorScheme.surfaceTint
+                )
+            }
+        }
+    }
+
 }
 
 
